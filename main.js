d7508466fa85a4a375593e903c27456471b32c54
Andrew5525 <109211005+Andrew5525@users.noreply.github.com>
2022-11-27 11:53:10 -0500

    Project, static menu

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const elementosCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel,{
+        duration:150
+    });
+});
---
+++ menu.js
@@ -0,0 +1,4 @@
+const nav = document.querySelector('.nav');
+window.addEventListener('scroll',function(){
+    nav.classList.toggle('active',window.scrollY)
+})
