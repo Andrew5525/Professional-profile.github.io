c8f98a7fdc9984d449e1ab5918d840a2ad120bc3
Andrew5525 <afvanegasb@correo.udistrital.edu.co>
2022-11-29 20:05:53 -0500

    service (hexagonal gallery)

--- main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const elementosCarousel = document.querySelectorAll('.carousel');
-    M.Carousel.init(elementosCarousel,{
-        duration:150
-    });
-});
